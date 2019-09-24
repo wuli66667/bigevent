ddb5afaf0735bed3d0a1ff4dc280a75043de7747
wuli66667 <1208934913@qq.com>
2019-09-24 21:11:44 +0800

    引入模态框js文件

--- web_back/js/utils/conFi.js
+++ web_back/js/utils/conFi.js
@@ -2,5 +2,7 @@ var BASE_URL = 'http://localhost:8000';
 var urList = {
     user_login: BASE_URL + '/admin/login',
     user_logout: BASE_URL + '/admin/logout',
-    user_getInfo: BASE_URL + '/admin/getuser'
+    user_getInfo: BASE_URL + '/admin/getuser',
+
+    user_category: BASE_URL + '/admin/category_search'
 };
