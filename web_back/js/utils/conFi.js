b3fa86090828a608fff02e636ca7313d87857b1f
wuli66667 <1208934913@qq.com>
2019-09-24 11:30:10 +0800

    补的js文件

---
+++ web_back/js/utils/conFi.js
@@ -0,0 +1,6 @@
+var BASE_URL = 'http://localhost:8000';
+var urList = {
+    user_login: BASE_URL + '/admin/login',
+    user_logout: BASE_URL + '/admin/logout',
+    user_getInfo: BASE_URL + '/admin/getuser'
+};
